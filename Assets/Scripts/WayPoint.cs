941f4fe5547fa58fc0a477b9b99d240576b80f64
HaydenLiu <huangxunliu@gmail.com>
2019-09-01 01:38:46 +1000

    test
    13091172
    UTS
    GameDevelopment
    PacMan

---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public GameObject wayPointsGo;
+    public float speed = 0.3f;
+    private readonly List<Vector3> wayPoints = new List<Vector3>();
+    private int index = 0;
+
+    private void Start()
+    {
+        foreach (Transform t in wayPointsGo.transform)
+        {
+            wayPoints.Add(t.position);
+        }
+    }
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        if (transform.position != wayPoints[index])
+        {
+            Vector2 temperation = Vector2.MoveTowards(transform.position, wayPoints[index], speed);
+            GetComponent<Rigidbody2D>().MovePosition(temperation);
+            Debug.Log(transform.position + ": " + wayPoints[index]);
+        }        else        {
+
+            index = (index + 1) % wayPoints.Count;            Debug.Log(index);        }
+
+        Vector2 dir = wayPoints[index] - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Pacm")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
